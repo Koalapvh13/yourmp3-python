4a6c4314d0698d2e714c10b08603f979fc9f0b47
Matheus Dias Vieira <matheuspvh13@gmail.com>
2020-09-04 00:03:54 -0400

    created function to get video title from url

---
+++ getData.py
@@ -0,0 +1,17 @@
+def getVideoTitle(url_link):
+    import urllib.request
+    import json
+    import urllib
+
+    params = {"format": "json", "url": url_link}
+    url = "https://www.youtube.com/oembed"
+    query_string = urllib.parse.urlencode(params)
+    url = url + "?" + query_string
+
+    title = ""
+    with urllib.request.urlopen(url) as response:
+        response_text = response.read()
+        data = json.loads(response_text.decode())
+        title = data["title"]
+    return title
+

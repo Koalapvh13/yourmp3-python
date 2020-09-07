151c95cf3977d7d7310c37fe224d834f68196575
Matheus Dias Vieira <matheuspvh13@gmail.com>
2020-09-07 13:21:12 -0400

    Implemented main download function

---
+++ main.py
@@ -0,0 +1,20 @@
+from DownloadMP3 import DownloadMP3
+from VideoInfo import getVideoTitle
+
+
+def download(videoID, savePath):
+    YTLINK = "https://www.youtube.com/watch?v=" + videoID
+    title = getVideoTitle(YTLINK)
+    if title is not "":
+        if DownloadMP3(videoID, title, savePath):
+            print("Baixado com sucesso!")
+        else:
+            print("Falha ao baixar o video!")
+    else:
+        print("Falha procurar o Titulo do Vídeo!")
+
+
+DIRECTORY = "./YourMP3-Downloads/"
+VIDEO_ID = "7o1FzLpRUKQ"
+
+download(VIDEO_ID, DIRECTORY)

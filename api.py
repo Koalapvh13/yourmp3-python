ba1f30855f50bd1713a3f3412b7faef449dc1019
Matheus Dias Vieira <matheuspvh13@gmail.com>
2020-09-12 17:14:55 -0400

    Add uvicorn call to api.py

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import BackgroundTasks, FastAPI
 from main import download, DIRECTORY
+import uvicorn
 
 app = FastAPI()
 
@@ -9,3 +10,8 @@ app = FastAPI()
 def read_root(video_id: str, bgTask: BackgroundTasks):
     bgTask.add_task(download, video_id, DIRECTORY)
     return {"response": "Download iniciado"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("api:app", port=8000, reload=True)
+
